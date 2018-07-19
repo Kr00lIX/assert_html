f7e519ff7af3b04165fb294ca0d4dcf4b4de2ae5
Anatoliy Kovalchuk <kr00lix@gmail.com>
2018-07-19 14:57:38 +0400

    init project

---
+++ lib/assert_html.ex
@@ -0,0 +1,18 @@
+defmodule AssertHtml do
+  @moduledoc """
+  Documentation for AssertHtml.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AssertHtml.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/assert_html_test.exs
@@ -0,0 +1,8 @@
+defmodule AssertHtmlTest do
+  use ExUnit.Case
+  doctest AssertHtml
+
+  test "greets the world" do
+    assert AssertHtml.hello() == :world
+  end
+end
