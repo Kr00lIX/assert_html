c330392a0fdd6fcf083a7f48d14efb252e32660d
Binh Tran <btbinhtran@gmail.com>
2019-04-19 00:43:51 -0700

    Try travis ci build fix.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule AssertHTML.MixProject do
     [
       app: :assert_html,
       version: @version,
-      elixir: ">= 1.6.0",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
